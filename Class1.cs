e89f428715bf273752e30594f047df98c14101eb
milly1110ning-dot <milly.1110ning@gmail.com>
2026-05-29 11:42:58 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+public class Person
+{
+    static public string GetName() 
+    {
+        return "許光漢";
+    }
+    static public string GetName(string title)
+    {
+        return title + "許光漢";
+    }
+    static public string GetName(string title,string dept)
+    {
+        return title + dept+"許光漢";
+    }
+    static public int GetNumber(int a)
+    {
+        return a;
+    }
+    static public float GetNumber(float)
+    {
+        return a;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Class_05_29_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person p1 =new Person();
+            //Console.WriteLine(p1.GetName());
+            //Console.WriteLine(p1.GetName("德明"));
+            Console.WriteLine(Person.GetName());
+            Console.WriteLine(Person.GetName("德明"));
+            Console.WriteLine(Person.GetName("德明", "資管"));
+            Console.WriteLine(Person.GetNumber(5));
+            Console.WriteLine(Person.GetNumber(5));
+        }
+    }
+}
